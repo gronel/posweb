344cf4e17a1e9413325d0b1f143ccb58d947b7fb
Ronel Gonzales <ronelgonzales5@gmail.com>
2026-01-27 21:56:11 +0800

    update

--- app/Models/UserRights.php
+++ app/Models/UserRights.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRights extends Model
 {
+    use HasFactory;
+
     protected $table = 'user_rights';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'UserId', 'id');
+    }
 }
