b615d700d3cd44b04649634c2fe3be30d7a2ba96
ronel gonzales <ronelgonzales5@gmail.com>
2026-02-06 15:25:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+            alias: {
+                ziggy: 'ziggy-js',
+                "bootstrap": path.join(__dirname, "node_modules/") + "bootstrap/dist/js/bootstrap.min.js",
+                find: "@vue/runtime-core",
+                replacement: "@vue/runtime-core/dist/runtime-core.esm-bundler.js",
+            }
+          },
     plugins: [
         laravel({
             input: ['resources/css/app.css',
