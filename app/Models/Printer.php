14daaf4fa5d9e598a733c8def4869f5b7c1eca30
drel <andrelmecal357@gmail.com>
2026-02-27 14:24:54 +0800

    printermodels

---
+++ app/Models/Printer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Printer extends Model
+{
+    protected $table = 'printer';
+
+    protected $fillable = [
+        'printerName',
+        'printerType',
+        'printerOrder',
+        'printerPrintServer',
+        'printerPrintPort',
+        'printerAccessType',
+        'printerUsername',
+        'printerPassword',
+        'printerShellExec',
+        'printerActive',
+        'printerUnit',
+        'printerLabels',
+        'printerDensity',
+        'printerDpi'
+    ];
+}
